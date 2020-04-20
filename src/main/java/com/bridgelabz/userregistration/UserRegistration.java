f4ef2f95dd993addad33d1bc4870b583a71acd21
PalakPartani <partanipalak16@gmail.com>
2020-04-20 02:26:47 -0700

    ADDED:feature to validate firstname

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -2,4 +2,10 @@ package com.bridgelabz.userregistration;
 
 public class UserRegistration {
 
+    static String USER_NAME = "^[A-Z]{1}[a-z]{2,}$";
+
+    public static boolean validInputName(String name) {
+        return name.matches(USER_NAME);
+    }
+
 }
--- src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -4,5 +4,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationTest {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        boolean firstName = UserRegistration.validInputName("Plk");
+        Assert.assertTrue(firstName);
+    }
 
+    @Test
+    public void givenFirstName_WhenImproper_ShouldReturnFalse() {
+        boolean firstName = UserRegistration.validInputName("plkk");
+        Assert.assertFalse(firstName);
+    }
 }
